e04e9763b469cac626d494bf4d565837ff98c8af
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2020-01-28 10:25:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+image_api_url = "http://genzcart-ikea-d-o-d.gamification-d3c0cb24e2b77f6869027abe3de4bca3-0001.sng01.containers.appdomain.cloud"
+
