c98ad2c9d050e0219aa6ec6056f8e84cf01776a4
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2020-02-01 22:54:43 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -147,7 +147,8 @@ def home_page():
         print(json.dumps(classes, indent=2))
         imgsrch_key = json.dumps(classes['images'][0]['classifiers'][0]['classes'][0]['class'])
         print("imgsrch_key is :", imgsrch_key)
-        qx = imgsrch_key.replace(' ','%')
+        qs = imgsrch_key.strip('"'))
+        qx = qs.replace(' ','%')
         qr = qx.replace("womens","women")
         if 'men' in qx and 'wo' not in qx:
           qy = qx.replace("men"," men")
